577361213f564850113da32026038474fc8eb854
yunyeop <yykim000922@gmail.com>
2021-07-29 17:50:00 +0900

    feat:initial commit (Back-End)

---
+++ Back-End/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { createServer } from "http";
+import router from "./routes";
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.resolve(
+    process.cwd(),
+    process.env.NODE_ENV == 'production' ? '.env' : '.env.dev'
+  ),
+});
+
+process.env.NODE_ENV = ( process.env.NODE_ENV && ( process.env.NODE_ENV ).trim().toLowerCase() == 'production' ) ? 'production' : 'development';
+
+const app: express.Application = express();
+const server = createServer(app);
+
+app.use('/', router);
+
+server.listen(process.env.PORT, () => {
+  console.info(`ON ${process.env.PORT}`);
+});
+
+export default server;
---
+++ Back-End/src/constants/httpStatus.ts
@@ -0,0 +1,6 @@
+export const HTTP_STATUS_CODE = {
+  OK: 200,
+  BAD_REQUEST: 400,
+  NOTFOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+} as const;
